7bae78c381db30b2b77b979b4f03db2ce0cce4df
kajaaam <kajaaam@stud.ntnu.no>
2025-03-05 16:07:01 +0100

    Success retrived data from frost API

--- src/fetch_data.py
+++ src/fetch_data.py
@@ -1,15 +1,16 @@
 import requests
 import pandas as pd
 
-def fetch_data(url, client_id, parameters):
-    response = requests.get(url, params=parameters, auth=(client_id, ""))
-    print("Status Code:", response.status_code)
-    print("Response Text:", response.text)
+def fetch_data(endpoint, parameters, client_id):
+    response = requests.get(endpoint, params=parameters, auth=(client_id, ""))
+    
     if response.status_code != 200:  
         print("Status Code:", response.status_code)
         print("Response Text:", response.text)
         print("Kunne ikke hente data")
-        return pd.DataFrame()  # Returner en tom DataFrame i stedet for å stoppe koden
+        return pd.DataFrame()
+    
+    print('Det funket!')
 
     data = response.json()["data"]
     målinger = []
@@ -17,7 +18,7 @@ def fetch_data(url, client_id, parameters):
     for måling in data:
         tidspunkt = måling["referenceTime"]
         stasjon = måling["sourceId"]
-
+        
         for verdi in måling["observations"]:
             målinger.append({"Stasjon": stasjon, "Tidspunkt": tidspunkt, "Temperatur": verdi["value"]})
 
@@ -26,8 +27,3 @@ def fetch_data(url, client_id, parameters):
     
     # Lagre som JSON
     df.to_json("../data/weather_data.json", orient="records", indent=4, force_ascii=False)
-
-    #Funker ikke 
-
-    
-
